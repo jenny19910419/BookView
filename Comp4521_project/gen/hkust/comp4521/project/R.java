b81f98b80c845ba546f157bbea5c7a954fb13a1f
jenny <jenny@wf108-093.ust.hk>
2014-03-10 16:51:02 +0800

    Add Action Bar Item

--- Comp4521_project/gen/hkust/comp4521/project/R.java
+++ Comp4521_project/gen/hkust/comp4521/project/R.java
@@ -21,12 +21,15 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int ic_action_circle=0x7f020000;
+        public static final int ic_action_search=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
         public static final int Password=0x7f080003;
         public static final int Username=0x7f080001;
+        public static final int action_circle=0x7f080008;
+        public static final int action_search=0x7f080007;
         public static final int action_settings=0x7f080006;
         public static final int loginBut=0x7f080005;
         public static final int loginHead=0x7f080000;
@@ -43,6 +46,8 @@ public final class R {
     }
     public static final class string {
         public static final int Login=0x7f050005;
+        public static final int action_circle=0x7f050008;
+        public static final int action_search=0x7f050007;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
