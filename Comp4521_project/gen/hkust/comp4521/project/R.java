bb70372d4429b72dda6c2ad4044ba66e530ab770
jenny <jenny@localhost>
2014-03-10 20:24:01 +0800

    Add Tab View in WholePage

--- Comp4521_project/gen/hkust/comp4521/project/R.java
+++ Comp4521_project/gen/hkust/comp4521/project/R.java
@@ -37,12 +37,18 @@ public final class R {
         public static final int myUsername=0x7f080002;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int activity_whole_page=0x7f030001;
+        public static final int activity_current=0x7f030000;
+        public static final int activity_home=0x7f030001;
+        public static final int activity_interesting=0x7f030002;
+        public static final int activity_main=0x7f030003;
+        public static final int activity_whole_page=0x7f030004;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
-        public static final int whole_page=0x7f070001;
+        public static final int current=0x7f070000;
+        public static final int home=0x7f070001;
+        public static final int interesting=0x7f070002;
+        public static final int main=0x7f070003;
+        public static final int whole_page=0x7f070004;
     }
     public static final class string {
         public static final int Login=0x7f050005;
@@ -52,6 +58,10 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
         public static final int password=0x7f050004;
+        public static final int title_activity_current=0x7f05000c;
+        public static final int title_activity_home=0x7f05000a;
+        public static final int title_activity_interesting=0x7f05000b;
+        public static final int title_activity_new=0x7f050009;
         public static final int title_activity_whole_page=0x7f050006;
         public static final int username=0x7f050003;
     }
