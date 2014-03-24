34af8a503782bf55b5f88cacaf90e482f7acde0e
YAN Yuchen <cirahnitex@gmail.com>
2014-03-24 15:36:48 +0800

    Add tabs.

---
+++ Comp4521_project/src/activity/MainActivity/MainActivityController.java
@@ -0,0 +1,44 @@
+package activity.MainActivity;
+
+import java.util.Observable;
+
+import model.User;
+import myUtil.Callable;
+
+/**
+ * a controller for the login activity
+ * @author cirah
+ *
+ */
+public class MainActivityController extends Observable
+{
+	public static final int STATE_DEFAULT = 0;
+	public static final int STATE_LOGIN_SUCCESS = 1;
+	public static final int STATE_LOGIN_FAIL = 2;
+	public static final int STATE_LOGGING_IN = 3; // waiting for server response
+	/**
+	 * perform login action
+	 * 
+	 * @param email
+	 * @param password
+	 */
+	public void login(String email,String password) {
+		this.setChanged();
+		this.notifyObservers(STATE_LOGGING_IN);
+		User.server_login(email, password, new Callable()
+		{
+			@Override
+			public void callback(final Object d) {
+				if(d == null) {
+					setChanged();
+					notifyObservers(STATE_LOGIN_FAIL);
+					return;
+				}
+				setChanged();
+				notifyObservers(STATE_LOGIN_SUCCESS);
+				
+			}
+
+		});
+	}
+}
