934cb5935053f440429d0148426c28494e9a286f
jzhaoaf <jzhaoaf@lbe026.ust.hk>
2014-03-25 08:16:50 +0800

    add freshpage

---
+++ Comp4521_project/src/activity/fresh/FreshPage.java
@@ -0,0 +1,24 @@
+package activity.fresh;
+
+
+import hkust.comp4521.project.R;
+import android.os.Bundle;
+
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+ 
+public class FreshPage extends Fragment {
+ 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+ 
+        View rootView = inflater.inflate(R.layout.fragment_freshpage, container, false);
+         
+        return rootView;
+    }
+   
+}
---
+++ Comp4521_project/src/activity/popular/PopPage.java
@@ -0,0 +1,23 @@
+package activity.popular;
+
+import hkust.comp4521.project.R;
+import android.os.Bundle;
+
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+ 
+public class PopPage extends Fragment {
+ 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+ 
+        View rootView = inflater.inflate(R.layout.fragment_poppage, container, false);
+         
+        return rootView;
+    }
+   
+}
