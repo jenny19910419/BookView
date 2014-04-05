280df0d1198c0b1a11608fc75476628141b9e4ef
jenny <jenny@wf111-136.ust.hk>
2014-04-05 10:01:03 +0800

    Set up search funtion with mocking test

--- Comp4521_project/gen/hkust/comp4521/project/R.java
+++ Comp4521_project/gen/hkust/comp4521/project/R.java
@@ -30,9 +30,9 @@ public final class R {
     public static final class id {
         public static final int Password=0x7f090003;
         public static final int Username=0x7f090001;
-        public static final int action_circle=0x7f09000d;
-        public static final int action_search=0x7f09000c;
-        public static final int action_settings=0x7f09000b;
+        public static final int action_circle=0x7f09000e;
+        public static final int action_search=0x7f09000d;
+        public static final int action_settings=0x7f09000c;
         public static final int bookname=0x7f090007;
         public static final int firstLine=0x7f090008;
         public static final int icon=0x7f090006;
@@ -42,6 +42,7 @@ public final class R {
         public static final int myUsername=0x7f090002;
         public static final int pager=0x7f09000a;
         public static final int secondLine=0x7f090009;
+        public static final int status_text=0x7f09000b;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- Comp4521_project/src/activity/search/SearchActivity.java
+++ Comp4521_project/src/activity/search/SearchActivity.java
@@ -1,40 +1,46 @@
 package activity.search;
 
+import java.util.ArrayList;
+
 import hkust.comp4521.project.R;
+import activity.bookview.BookViewAdaptor;
+import activity.bookview.BookViewInfo;
 import android.app.ListActivity;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
 import android.widget.SearchView;
 
 /*public class SearchActivity extends ListActivity {
-	
-	
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
-	    setContentView(R.layout.fragment_search);
-	    // Get the SearchView and set the searchable configuration
-	    SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-	    //SearchView searchView = (SearchView) findItemById(R.id.);
-	    //searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-	    //searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
 
-	    // Get the intent, verify the action and get the query
-	    Intent intent = getIntent();
-	    if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-	      String query = intent.getStringExtra(SearchManager.QUERY);
-	      doMySearch(query);
-	    }
-	}
 
-	private void doMySearch(String query) {
-		// TODO Auto-generated method stub
-		
-	}
+ @Override
+ public void onCreate(Bundle savedInstanceState) {
+ super.onCreate(savedInstanceState);
+ setContentView(R.layout.fragment_search);
+ // Get the SearchView and set the searchable configuration
+ SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+ //SearchView searchView = (SearchView) findItemById(R.id.);
+ //searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+ //searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
 
-}*/
+ // Get the intent, verify the action and get the query
+ Intent intent = getIntent();
+ if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+ String query = intent.getStringExtra(SearchManager.QUERY);
+ doMySearch(query);
+ }
+ }
+
+ private void doMySearch(String query) {
+ // TODO Auto-generated method stub
+
+ }
+
+ }*/
 
 /*
  * class that performs searches based on a query string and presents the search results.
@@ -42,26 +48,59 @@ import android.widget.SearchView;
  * Search your data
  * Presenting your result
  */
-public class SearchActivity extends ListActivity{
-	
+public class SearchActivity extends ListActivity {
+
+	private BookViewInfo[] searchResult;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
-	    setContentView(R.layout.fragment_search);
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.fragment_search);
 
-	    // Get the intent, verify the action and get the query
-	    Intent intent = getIntent();
-	    if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-	      String query = intent.getStringExtra(SearchManager.QUERY);
-	      doMySearch(query);
-	    }
+		// Get the intent, verify the action and get the query
+		Intent intent = getIntent();
+		handleIntent(getIntent());
+
+		// display the result
+		BookViewAdaptor adapter = new BookViewAdaptor(this, searchResult);
+		setListAdapter(adapter);
+	}
+
+	private void handleIntent(Intent intent) {
+		if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+			String query = intent.getStringExtra(SearchManager.QUERY);
+			doMySearch(query);
+		}
 	}
 
 	/*
-	 * @param do the search according to your query
+	 * @param do the search according to your query, suppose the query is the
+	 * name of the book
 	 */
 	private void doMySearch(String query) {
 		// TODO Auto-generated method stub
-		
+
+		BookViewInfo bookViewOne = new BookViewInfo("first org",
+				"first review", R.drawable.testpor1, 1, "bookOneName");
+		BookViewInfo bookViewTwo = new BookViewInfo("second org",
+				"second review", R.drawable.testpor2, 2, "BookTwoName");
+
+		BookViewInfo[] bookViewArray = { bookViewOne, bookViewTwo };
+		searchResult = bookViewArray;
 	}
+
+	public void onListItemClick(ListView l, View v, int position, long id) {
+		// call detail activity for clicked entry
+	}
+	
+	/*
+	 * Should a user start a new search from within your search activity, 
+	 * Android would recycle the instance and call the method onNewIntent()
+	 *  with the new search intent as its parameter.
+	 */
+	public void onNewIntent(Intent intent) { 
+		setIntent(intent);
+		handleIntent(intent);
+	}
+
 }
