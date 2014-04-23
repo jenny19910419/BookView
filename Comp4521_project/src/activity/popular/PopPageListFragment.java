0b83db59c0e89822312be1c098fc4c69b21b9ff1
wangbo <admin@192.168.1.113>
2014-04-23 13:26:09 +0800

    poppage

---
+++ Comp4521_project/src/activity/popular/PopPageListFragment.java
@@ -0,0 +1,39 @@
+package activity.popular;
+
+import hkust.comp4521.project.R;
+import MockBookViewList.MockBookViewList;
+import activity.bookview.BookViewAdaptor;
+import activity.bookview.BookViewInfo;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+
+public class PopPageListFragment extends ListFragment
+{
+	private static final String TAG = "PopPageList";
+	
+	@Override
+	  public void onActivityCreated(Bundle savedInstanceState) {
+	    super.onActivityCreated(savedInstanceState);
+	   //wangbo replace the test instance
+	    MockBookViewList mkbookviewlist= new MockBookViewList();
+	    BookViewAdaptor adapter = new BookViewAdaptor(getActivity(), mkbookviewlist.bookViewArray2);
+
+	    setListAdapter(adapter);
+	  }
+	
+	
+	 @Override
+	public void onListItemClick(ListView l, View v, int position, long id) {
+	    BookViewInfo item = (BookViewInfo) getListAdapter().getItem(position);
+	    //Toast.makeText(this, item + " selected", Toast.LENGTH_LONG).show();
+	 }
+
+}
+
