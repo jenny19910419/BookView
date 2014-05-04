1d6677a46aacd45754a59a359e44164799152b18
YAN Yuchen <cirahnitex@gmail.com>
2014-05-04 13:32:02 +0800

    add book search function

--- Comp4521_project/src/model/Book.java
+++ Comp4521_project/src/model/Book.java
@@ -1,9 +1,36 @@
 package model;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.util.Log;
+import myUtil.Callable;
+import myUtil.Server;
+
 public class Book extends Data
 {
+	public static final String TAG = "Book";
 	public String name = "";
 	public String preview = "";
 	public String author = "";
 	public String ISBN = "";
+	
+	public static void server_search(String query, final Callable callback) {
+		Server.post("Book/search", new String[]{query}, new Callable() {
+
+			@Override
+			public void callback(Object d) {
+				// TODO Auto-generated method stub
+				if(! (d instanceof JSONArray)) {
+					Log.e(TAG, "server_search returns a non-array object");
+					callback.callback(null);
+				}
+				
+				Book[] rtn = Data.from_json_arr(Book.class, (JSONArray)d);
+				callback.callback(rtn);
+			}
+			
+		});
+	}
 }
