a62c0441667bc0192b520e40944547162a789762
jenny <jenny@wf230-167.ust.hk>
2014-05-12 15:42:38 +0800

    change homepage layout

--- Comp4521_project/src/activity/home/HomePage.java
+++ Comp4521_project/src/activity/home/HomePage.java
@@ -1,20 +1,34 @@
 package activity.home;
 
+import model.User;
 import hkust.comp4521.project.R;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class HomePage extends Fragment {
+	
 	 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-    	View rootView = inflater.inflate(R.layout.fragment_bookview, container, false);
+    	View rootView = inflater.inflate(R.layout.fragment_homepage, container, false);
+    	
+    	//ImageView portrait = (ImageView) getView().findViewById(R.id.homepage_icon);
+    	//BitmapDrawable d = new BitmapDrawable(User.get_active_user().imageUrl);
+    	//portrait.setImageDrawable(d);
+    	
+    	//EditText email = (EditText) getView().findViewById(R.id.homepage_email);
+    	//email.setText(User.get_active_user().email);
+		//EditText username = (EditText) getView().findViewById(R.id.homepage_username);
+		//username.setText(User.get_active_user().name);
 
         return rootView;
     }
--- Comp4521_project/src/activity/social/AddBookBuddyActivity.java
+++ Comp4521_project/src/activity/social/AddBookBuddyActivity.java
@@ -1,16 +1,37 @@
 package activity.social;
 
+import model.User;
 import hkust.comp4521.project.R;
 import android.app.Activity;
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class AddBookBuddyActivity extends ListActivity {
+public class AddBookBuddyActivity extends Activity {
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		//setContentView(R.layout.);
+		setContentView(R.layout.addfollowingview);
+		
+		EditText email = (EditText) findViewById(R.id.editFollowinggEmailName);
+		Button searchButton = (Button) findViewById(R.id.editFollowingButton);
+		
+		searchButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				//check if the email exists int the database;
+				
+			}
+			
+		});
+		
+		
 	}
 	
 }
